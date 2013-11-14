5f6b8c59017974b61c6204d1fe0281c1c2016748
Chris Aumann <me@chr4.org>
2013-11-14 17:53:00 +0100

    add basic test

---
+++ files/default/tests/minitest/default_test.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: sshd
+# Test:: default
+#
+# Copyright 2013, Chris Aumann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+require File.expand_path('../support/helpers', __FILE__)
+
+describe 'sshd::default' do
+  include Helpers::TestHelper
+
+  it 'should install openssh-server' do
+    package(node['sshd']['package']).must_be_installed
+  end
+
+  it 'should create sshd_config according to specifications' do
+    file(node['sshd']['config_file']).must_include('Port 1234')
+  end
+
+  it 'should start openssh-server service' do
+    service(node['sshd']['service_name']).must_be_running
+  end
+
+  it 'should enable openssh-server service' do
+    service(node['sshd']['service_name']).must_be_enabled
+  end
+end
