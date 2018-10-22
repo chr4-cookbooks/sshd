285c4926edc20e7ab025b7d0af828c5472ea33bc
John Martin <johnny@iamjohnnym.com>
2018-10-22 14:13:26 -0700

    Updated changelog

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'me@chr4.org'
 license          'GPL-3.0-or-later'
 description      'Installs/Configures sshd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.3.1'
+version          '1.4.0'
 source_url       'https://github.com/chr4-cookbooks/sshd' if respond_to?(:source_url)
 issues_url       'https://github.com/chr4-cookbooks/sshd/issues' if respond_to?(:issues_url)
 
