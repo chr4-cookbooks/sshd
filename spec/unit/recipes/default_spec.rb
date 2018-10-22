15841f38abf1064db0563356339c3b7dbb29c818
iamjohnnym <johnny@iamjohnnym.com>
2018-10-22 14:06:23 -0700

    Update Tests and travis (#4)
    * Fixed some liniting issues for `rubocop`
    * specify trusty in travis
    * matcher for custom resource
    * implement the community TESTING.md
    * implemented `delivery`

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,35 +7,28 @@
 require 'spec_helper'
 
 describe 'sshd::default' do
-  context 'When all attributes are default, on Ubuntu 14.04' do
+  context 'when run on Ubuntu 18.04' do
     let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04')
-      runner.converge(described_recipe)
+      ChefSpec::SoloRunner.new(
+        platform: 'ubuntu',
+        version: '18.04')
+                          .converge(described_recipe)
     end
 
     it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+      expect { :chef_run }.to_not raise_error
     end
 
-    describe 'configuration file' do
-      it { expect(chef_run).to render_file('/etc/ssh/sshd_config') }
+    it 'installs the ssh server package' do
+      expect(chef_run).to install_package('openssh-server')
+    end
 
-      it 'Port is before ListenAddress when both are defined' do
-        chef_run.node.normal['sshd']['sshd_config'] = {
-          'Port' => 2222,
-          'ListenAddress' => '0.0.0.0'
-        }
-        chef_run.converge(described_recipe)
-        expect(chef_run).to render_file('/etc/ssh/sshd_config').with_content(/Port 2222.+ListenAddress 0\.0\.0\.0/m)
-      end
+    it 'creates ssh directory' do
+      expect(chef_run).to create_directory('/etc/ssh')
+    end
 
-      it 'has multiple Port entries when an array is given' do
-        chef_run.node.normal['sshd']['sshd_config'] = {
-          'Port' => [2222, 22]
-        }
-        chef_run.converge(described_recipe)
-        expect(chef_run).to render_file('/etc/ssh/sshd_config').with_content("Port 2222\nPort 22\n")
-      end
+    it 'executes custom resource' do
+      expect(chef_run).to create_openssh_server('/etc/ssh/sshd_config')
     end
   end
 end
---
+++ test/cookbooks/sshd-test/attributes/default.rb
@@ -0,0 +1,108 @@
+#
+# Cookbook Name:: sshd
+# Attributes:: default
+#
+# Copyright 2012, Chris Aumann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# The package to install
+default['sshd']['package'] =
+  case node['platform']
+  when 'archlinux', 'suse'
+    'openssh'
+  else
+    'openssh-server'
+  end
+
+# Path to 'sshd' executable
+default['sshd']['sshd_path'] =
+  case node['platform']
+  when 'redhat', 'centos'
+    node['platform_version'].to_i >= 7 ? '/sbin/sshd' : '/usr/sbin/sshd'
+  else
+    '/usr/sbin/sshd'
+  end
+
+# Path to 'sshd_config' configuration file
+default['sshd']['config_file'] =
+  case node['platform_family']
+  when 'mac_os_x'
+    '/etc/sshd_config'
+  else
+    '/etc/ssh/sshd_config'
+  end
+
+# OpenSSH service name
+default['sshd']['service_name'] =
+  case node['platform_family']
+  when 'debian'
+    'ssh'
+  else
+    'sshd'
+  end
+
+# Define sshd_config attributes
+default['sshd']['sshd_config'] = {
+  'Port' => 22,
+  'Protocol' => 2,
+  'AcceptEnv' => 'LANG LC_*',
+  'HostKey' => %w(/etc/ssh/ssh_host_rsa_key
+                  /etc/ssh/ssh_host_ed25519_key
+                  /etc/ssh/ssh_host_dsa_key
+                  /etc/ssh/ssh_host_ecdsa_key),
+  'PasswordAuthentication' => 'yes',
+  'ChallengeResponseAuthentication' => 'no',
+  'X11Forwarding' => 'yes',
+  'UsePAM' => 'yes',
+  'SyslogFacility' => 'AUTH',
+  'GSSAPIAuthentication' => 'no',
+}
+
+# sshd_config permissions
+default['sshd']['sshd_config_mode'] =
+  case node['platform_family']
+  when 'debian', 'mac_os_x'
+    '0o644'
+  when 'rhel', 'fedora'
+    '0o600'
+  end
+
+# Initialize sftp subsystem
+default['sshd']['sshd_config']['Subsystem'] =
+  case node['platform_family']
+  when 'debian'
+    'sftp /usr/lib/openssh/sftp-server'
+  when 'rhel', 'fedora'
+    'sftp /usr/libexec/openssh/sftp-server'
+  when 'mac_os_x'
+    'sftp /usr/libexec/sftp-server'
+  end
+
+case node['platform_family']
+when 'debian'
+  # On debian-like systems, pam takes care of the motd
+  default['sshd']['sshd_config']['PrintMotd'] = 'no'
+
+when 'rhel', 'fedora'
+  default['sshd']['sshd_config']['SyslogFacility'] = 'AUTHPRIV'
+  default['sshd']['sshd_config']['GSSAPIAuthentication'] = 'yes'
+  default['sshd']['sshd_config']['AcceptEnv'] = 'LANG LANGUAGE LC_* XMODIFIERS'
+
+when 'mac_os_x'
+  default['sshd']['sshd_config']['SyslogFacility'] = 'AUTHPRIV'
+  default['sshd']['sshd_config']['UsePrivilegeSeparation'] = 'sandbox'
+  default['sshd']['sshd_config']['X11Forwarding'] = 'no'
+end
